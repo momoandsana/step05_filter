f3411838107235cf4a75d46a79950e16a9135912
hoseok <swiftie52@naver.com>
2024-09-27 10:32:27 +0900

    log 관련코드 추가

--- src/main/java/ex0926/filter/SampleFilter.java
+++ src/main/java/ex0926/filter/SampleFilter.java
@@ -1,11 +1,23 @@
-
+package ex0926.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
-import java.util.logging.LogRecord;
 
+@WebFilter(urlPatterns = "/*",
+        initParams = {
+                @WebInitParam(name = "fname", value = "a.jpg"),
+                @WebInitParam(name = "saveDir", value = "C:\\Edu\\log"),
+                @WebInitParam(name = "encoding", value = "UTF-8")
+        })
 public class SampleFilter implements Filter {
+
+    // LogFactory를 사용한 Logger
+    Log log = LogFactory.getLog(SampleFilter.class);
 
     public SampleFilter() {
         System.out.println("SampleFilter.SampleFilter constructor");
@@ -14,25 +26,34 @@ public class SampleFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("SampleFilter.init");
+        log.debug("초기화입니다");
+
+        // 초기화 파라미터 가져오기
+        String encoding = filterConfig.getInitParameter("encoding");
+        String saveDir = filterConfig.getInitParameter("saveDir");
+
+        log.debug("저장 디렉토리: " + saveDir + ", 인코딩: " + encoding);
     }
 
     @Override
     public void destroy() {
         System.out.println("SampleFilter.destroy");
+        log.debug("필터가 파괴되었습니다");
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res ,FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
         // 사전처리(공통기능)
+        log.info("사전처리입니다: 클라이언트 IP - " + req.getRemoteAddr());
 
+        // 요청이 처리될 때마다 로그 기록
+        log.info("클라이언트가 접속했습니다: " + req.getRemoteAddr() + ", 요청 시간: " + System.currentTimeMillis());
 
-        System.out.println("사전처입니다");
-        chain.doFilter(req,res);// 다음 타겟대상 호출(filter or servlet or jsp)
+        // 다음 타겟 대상 호출 (filter, servlet 또는 jsp)
+        chain.doFilter(req, res);
 
-
-        System.out.println("사후처리입니다");
         // 사후처리(공통기능)
-
-
+        log.info("사후처리입니다");
     }
 }
--- src/main/java/ex0926/log/LogTest.java
+++ src/main/java/ex0926/log/LogTest.java
@@ -1,10 +1,13 @@
 package ex0926.log;
 
-import org.apache.log4j.Logger;
+import jakarta.servlet.annotation.WebFilter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class LogTest {
-    // log4j 1.x에서는 LogManager 대신 Logger.getLogger를 사용해야 합니다.
-    Logger log = Logger.getLogger(LogTest.class);
+    // Log4j 2.x Logger
+    private static final Logger log = LogManager.getLogger(LogTest.class);
 
     public static void main(String[] args) {
         System.out.println("*** 로그 테스트 시작");
@@ -15,7 +18,7 @@ public class LogTest {
     }
 
     public void test() {
-        log.trace("trace 관련 로그");  // log4j 1.x에서는 trace를 지원하지 않음
+        log.trace("trace 관련 로그");
         log.debug("debug 관련 로그");
         log.info("info 관련 로그");
         log.warn("warn 관련 로그");
